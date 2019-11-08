db043a1109cfbb75049ed57de1b3ffacbb28e2e0
Lenin Samaniego <leningsv@hotmail.com>
2019-11-08 15:12:37 -0500

    MANEJO DE STATES

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = props => {
+    const [personsState, setPersonsState] = useState({
+        persons: [{
+            name: 'Lenin',
+            age: '31'
+        }, {
+            name: 'Diana',
+            age: '29'
+        }, {
+            name: 'Liz',
+            age: '27'
+        }],
+        otherState: 'Some value'
+    });
+    const switchNameHandler = (newName) => {
+        console.log('Was clicked!');
+        // DONT DO THIS this.state.persons[0].name = 'LENIN';
+        setPersonsState({
+            persons: [{
+                name: newName,
+                age: '31'
+            }, {
+                name: 'Diana',
+                age: '29'
+            }, {
+                name: 'Liz',
+                age: '27'
+            }]
+        })
+    };
+    return (
+        <div className="App">
+            <h1>React project</h1>
+            <p>Esto es real</p>
+            Mas ineficiente ya que react hace un rerender. switchNameHandler.bind(this, 'Lenin 1')
+            Recomendada switchNameHandler.bind(this, 'Lenin 2')
+            <button onClick={switchNameHandler.bind(this, 'Lenin 1')}>Swith Name</button>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}
+                click={switchNameHandler.bind(this, 'Lenin 2')}>
+                My Hobbies: Racing
+            </Person>
+            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}
+                    click={()=> switchNameHandler('Lenin 4')}/>
+        </div>
+    );
+};
 export default App;
