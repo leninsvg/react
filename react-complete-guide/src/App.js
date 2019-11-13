bb7dade486682ca22dbf7a1235556e2b7061af3e
Lenin Samaniego <leningsv@hotmail.com>
2019-11-13 15:40:16 -0500

    ifs y  listas

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
             {name: 'Manu', age: 29},
             {name: 'Stephanie', age: 26}
         ],
-        otherState: 'some other value'
+        otherState: 'some other value',
+        showPersons: false
     };
     switchNameHandler = (newName) => {
         console.log('Was clicked!');
@@ -44,6 +45,17 @@ class App extends Component {
         })
     };
 
+    deletePersonHandler(index) {
+        this.state.persons.splice(index, 1);
+        this.setState(this.state.persons);
+    }
+
+    togglePersonsHandler = () => {
+        this.setState({
+            showPersons: !this.state.showPersons
+        });
+    };
+
     render() {
         const style = {
             backgroundColor: 'white',
@@ -52,25 +64,41 @@ class App extends Component {
             padding: '8px',
             cursor: 'pointer'
         };
+        let persons = null;
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    {this.state.persons.map((person, index) => {
+                        return <Person
+                            click={() => this.deletePersonHandler(index)}
+                            name={person.name}
+                            age={person.age}/>;
+                    })}
+                    {/*<Person name={this.state.persons[0].name} age={this.state.persons[0].age}*/}
+                    {/*        change={this.nameChangedHandler}/>*/}
+                    {/*<Person name={this.state.persons[1].name} age={this.state.persons[1].age}*/}
+                    {/*        click={this.switchNameHandler.bind(this, 'Lenin 2')}*/}
+                    {/*        change={this.nameChangedHandler}*/}
+                    {/*>*/}
+                    {/*    My Hobbies: Racing*/}
+                    {/*</Person>*/}
+                    {/*<Person name={this.state.persons[2].name} age={this.state.persons[2].age}*/}
+                    {/*        click={() => this.switchNameHandler.bind(this, 'Lenin 4')}*/}
+                    {/*        change={this.nameChangedHandler}/>*/}
+
+                </div>
+            );
+        }
         return (
             <div className="App">
                 <h1>React project</h1>
                 <p>Esto es real</p>
                 Mas ineficiente ya que react hace un rerender. switchNameHandler.bind(this, 'Lenin 1')
                 Recomendada switchNameHandler.bind(this, 'Lenin 2')
-                <button onClick={this.switchNameHandler.bind(this, 'aaa')}
-                    style={style}
-                >Swith Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}
-                        change={this.nameChangedHandler}/>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
-                        click={this.switchNameHandler.bind(this, 'Lenin 2')}
-                        change={this.nameChangedHandler}
-                >
-                    My Hobbies: Racing
-                </Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}
-                        click={() => this.switchNameHandler.bind(this, 'Lenin 4')} change={this.nameChangedHandler}/>
+                <button onClick={this.togglePersonsHandler}
+                        style={style}
+                >toggle content</button>
+                {persons}
             </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
